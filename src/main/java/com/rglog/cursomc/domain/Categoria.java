1e34a81b4fab5a82be790fd08dd63fee78c43a6c
Sidney Palmeira <palmeira.sidney@gmail.com>
2022-07-13 11:24:43 -0300

    Produto e Associação muitos para muitos.

--- src/main/java/com/rglog/cursomc/domain/Categoria.java
+++ src/main/java/com/rglog/cursomc/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.rglog.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -16,6 +19,9 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 		
@@ -58,6 +64,14 @@ public class Categoria implements Serializable{
 		super();
 		this.id = id;
 		this.nome = nome;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 }
