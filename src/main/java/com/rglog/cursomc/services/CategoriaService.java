2412dd4acfd57af75cf886e8bde14e83372a3044
Sidney Palmeira <palmeira.sidney@gmail.com>
2022-07-13 12:17:52 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/rglog/cursomc/services/CategoriaService.java
+++ src/main/java/com/rglog/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.rglog.cursomc.domain.Categoria;
 import com.rglog.cursomc.repositories.CategoriaRepository;
+import com.rglog.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -18,6 +19,7 @@ public class CategoriaService {
 		
 		Optional<Categoria> obj = repo.findById(id);
 		
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
