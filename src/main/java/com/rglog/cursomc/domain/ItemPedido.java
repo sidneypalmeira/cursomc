c764b2814822f5ec8df3754dd2f6d7ea3248a7b0
Sidney Palmeira <palmeira.sidney@gmail.com>
2022-07-14 21:34:37 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/rglog/cursomc/domain/ItemPedido.java
@@ -0,0 +1,89 @@
+package com.rglog.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Entity
+public class ItemPedido implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId	
+	private ItemPedidoPK id = new ItemPedidoPK();
+	
+	private Double desconto;
+	private Integer quantidade;
+	private double preco;
+
+	public ItemPedido() {
+	}
+
+	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, double preco) {
+		super();
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		this.desconto = desconto;
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedido other = (ItemPedido) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+	
+	public Produto getProduto() {
+		return id.getProduto();
+	}
+	
+	public ItemPedidoPK getId() {
+		return id;
+	}
+
+	public void setId(ItemPedidoPK id) {
+		this.id = id;
+	}
+
+	public Double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(Double desconto) {
+		this.desconto = desconto;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+}
